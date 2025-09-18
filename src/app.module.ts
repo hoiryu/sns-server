d518d4a952272e48aa1898609c3ab44958950e8d
hoiryu <comzkow@gmail.com>
2025-09-18 13:02:54 +0900

    초기 셋업

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModel } from '~posts/entities/posts.entity';
+import { PostsModule } from '~posts/posts.module';
+import { AppController } from '~src/app.controller';
+import { AppService } from '~src/app.service';
+
+@Module({
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: ['.env.local', '.env'],
+		}),
+		PostsModule,
+		TypeOrmModule.forRootAsync({
+			inject: [ConfigService],
+			useFactory: (config: ConfigService) => ({
+				type: 'postgres',
+				host: config.get('DB_HOST', '127.0.0.1'),
+				port: config.get<number>('DB_PORT', 5432),
+				username: config.get('DB_USERNAME', 'postgres'),
+				password: config.get('DB_PASSWORD'),
+				database: config.get('DB_DATABASE', 'postgres'),
+				entities: [PostsModel],
+				synchronize: true,
+			}),
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
+})
+export class AppModule {}
